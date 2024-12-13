3eacd4164beb70c4308c3251b6a70844367d979a
dlotiw <witold.brzykcy@student.ukw.edu.pl>
2024-12-13 11:38:49 +0100

    Added missing file

---
+++ gry_plasznowe/src/store.js
@@ -0,0 +1,14 @@
+import { reactive } from 'vue'
+
+export const inputData = reactive({
+    Liczba_graczy: -1,
+    koszt1: -1,
+    koszt2: -1,
+    Przesten: -1,
+    Samodzielny_druk: -1,
+    Duzy_stol: -1,
+    typ_gry: -1,
+    mechanika_gry_l: -1,
+    mechanika_gry_s: -1,
+    mechanika_gry_p: -1
+});
